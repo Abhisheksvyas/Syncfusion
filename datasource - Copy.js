b58e3a9b4381268d6c7ebb0eba24acfe821f1e17
Abhisheksvyas <abhisheksvyas@gmail.com>
2020-08-01 18:57:22 +0530

    Add files via upload

---
+++ datasource - Copy.js
@@ -0,0 +1,16 @@
+
+var unscheduledData = [
+    {
+        TaskId: 1, TaskName: 'Task 1', StartDate: new Date('01/03/2019'),
+        EndDate: new Date('01/08/2019'), Duration: '5', TaskType: ''
+    },
+    {
+        TaskId: 2, TaskName: 'Task 2', Duration: '5', TaskType: 'Task with duration only'
+    },
+    {
+        TaskId: 3, TaskName: 'Task 3', StartDate: new Date('01/03/2019'), TaskType: 'Task with start date only'
+    },
+    {
+        TaskId: 4, TaskName: 'Task 4', EndDate: new Date('01/08/2019'), TaskType: 'Task with end date only'
+    },
+];
